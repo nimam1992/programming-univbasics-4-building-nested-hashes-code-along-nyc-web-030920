e1faeb2af1b53d375ab93d8bafb274670b0fb9ea
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-01-15 01:54:49 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,5 +27,3 @@ epic_tragedy[:capulet][:heroine][:status]='dead'
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-
-puts bonus
